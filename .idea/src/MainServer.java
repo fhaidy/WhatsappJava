4ccc3844e603e7823dd46f6ad5c1146bde25e43f
fhaidy <fhaidy@outlook.com>
2020-05-09 16:16:05 -0300

    Finishing Server

--- .idea/src/MainServer.java
+++ .idea/src/MainServer.java
@@ -1,5 +1,6 @@
 package sockets;
 
+import java.io.IOException;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -9,62 +10,72 @@ public class MainServer {
 
     public static void main(String[] args) {
 
-        ServerSocket server;
-        Socket cliente = new Socket();
-        Scanner in;
-        PrintStream saida;
-        Boolean start = true;
+        ServerSocket server = createServerSocket();
+        if (server.isClosed()) return;
 
+        Socket client = establishConnection(server);
+        if (client.isClosed()) return;
+
+        while (client.isConnected()) {
+            try {
+                Scanner input = new Scanner(client.getInputStream());
+                if (input.hasNext()){
+                    printInputToServer(input);
+                    printFeedbackToClient(client);
+                }else{
+                    closeConnection(server, client);
+                }
+            } catch (Exception e) {
+                System.out.println("Error communicating \n"
+                        + "Log: " + e.getMessage());
+                return;
+            }
+        }
+    }
+
+    private static void printInputToServer(Scanner in) {
+        String input = in.nextLine();
+        System.out.println("Client: " + input);
+    }
+
+    private static void printFeedbackToClient(Socket client) throws IOException {
+        PrintStream out;
+        out = new PrintStream(client.getOutputStream());
+        out.println("Message Received");
+    }
+
+    private static Socket establishConnection(ServerSocket server) {
+        Socket client;
+        try {
+            System.out.println("Waiting...");
+            client = server.accept();
+            System.out.println("Conexão estabelecida \n"  + client.getInetAddress().getHostAddress());
+        } catch (Exception e) {
+            System.out.println("Error connecting  \n" + "Log: " + e.getMessage());
+            return null;
+        }
+        return client;
+    }
+
+    private static ServerSocket createServerSocket() {
+        ServerSocket server;
         try {
             server = new ServerSocket(9876);
         } catch (Exception e) {
             System.out.println("Erro ao criar servidor na porta 9876 \n"
                     + "Log: " + e.getMessage());
-            return;
+            return null;
         }
-        try {
-            System.out.println("Aguardando conexão...");
-            cliente = server.accept();
-            System.out.println("Conexão estabelecida \n"  + cliente.getInetAddress().getHostAddress());
-        } catch (Exception e) {
-            System.out.println("Erro ao conectar com cliente :C  \n" + "Log: " + e.getMessage());
-            return;
-        }
-        while (start) {
-            start = cliente.isConnected();
-            try {
-                in = new Scanner(cliente.getInputStream());
-                String entrada;
-                if (in.hasNext()){
-                    entrada = in.nextLine();
-                    System.out.println("Client: " + entrada);
-                    saida = new PrintStream(cliente.getOutputStream());
-                    saida.println("Message Received");
-                }
-            } catch (Exception e) {
-                System.out.println("Erro de comunicação :C \n"
-                        + "Log: " + e.getMessage());
-                return;
-            }
-        }
-        try{
-            closeConnection(server, cliente);
-        }catch (Exception e) {
-            System.out.println("Erro ao encerrar conexão :C \n" + "Log: " + e.getMessage());
-        }
+        return server;
     }
 
     private static void closeConnection(ServerSocket server, Socket cliente) {
         try {
+            server.close();
             cliente.close();
-            if (cliente.getInetAddress().getHostAddress().equalsIgnoreCase("127.0.0.1")) {
-                System.out.println("Eduardo se conectou :P");
-                server.close();
-                cliente.close();
-
-            }
+            System.out.println("Connection closed");
         } catch (Exception e) {
-            System.out.println("Erro ao encerrar conexão :C \n"
+            System.out.println("Error finishing connection\n"
                     + "Log: " + e.getMessage());
         }
     }
